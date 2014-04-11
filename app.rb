1fb0fafa9975c5ad7577ada961b835b5cf3d9bc2
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-12 00:54:52 +0900

    fix app.rb|parser.rb / table作成をparser.rbに移行

--- app.rb
+++ app.rb
@@ -8,35 +8,6 @@ require_relative 'models/init'
 
 get '/' do
   @parser = Parse.new
-  list = @parser.parse
-  @table = ""
-  @place = [
-    "ITC事務室入り口横(壁側)",
-    "ITC 入り口横左",
-    "ITC 入り口横右",
-    "λ11左",
-    "λ11右",
-    "λ21",
-    "λ18",
-    "o17",
-    "i18",
-    "e17",
-    "k18",
-    "k教室棟2階",
-    "i教室棟2階",
-    "メディアセンター右",
-    "メディアセンター左"
-  ]
-  list.each {|s|
-    @table += "<tr>"
-    @table += "<td>" + @place[0] + "</td>"
-    @place.delete_at(0)
-    if (s[0].sub("%","")).to_i <= 25
-      @table+="<td><b style=\"color:red\">"+s[0]+"</b></td>"
-    else
-      @table+="<td>"+s[0]+"</td>"
-    end
-    @table += "</tr>"
-  }
+  @table = @parser.parse
   haml :index
 end
--- models/parser.rb
+++ models/parser.rb
@@ -13,7 +13,35 @@ class Parse
       l[i] = l[i][2..4]
     }
     n_list = [l[10],l[13],l[14],l[8],l[9],l[7],l[4],l[3],l[2],l[1],l[0],l[5],l[6],l[12],l[13]]
-    p n_list
-    return n_list
+    res = ""
+    place = [
+      "ITC事務室入り口横(壁側)",
+      "ITC 入り口横左",
+      "ITC 入り口横右",
+      "λ11左",
+      "λ11右",
+      "λ21",
+      "λ18",
+      "o17",
+      "i18",
+      "e17",
+      "k18",
+      "k教室棟2階",
+      "i教室棟2階",
+      "メディアセンター右",
+      "メディアセンター左"
+    ]
+    n_list.each {|s|
+      res += "<tr>"
+      res += "<td>" + place[0] + "</td>"
+      place.delete_at(0)
+      if (s[0].sub("%","")).to_i <= 25
+        res+="<td><b style=\"color:red\">"+s[0]+"</b></td>"
+      else
+        res+="<td>"+s[0]+"</td>"
+      end
+      res += "</tr>"
+    }
+    return res
   end
 end
