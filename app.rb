99bd97c807ca209347816dae7122123618a1a0a4
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-15 20:12:50 +0900

    fix app.rb / getter.rbをrequire

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require_relative 'models/init'
 
 class Server < Sinatra::Base
   get '/' do
+    require_relative 'models/getter'
     @parser = Parse.new
     @table = @parser.parse
     @date = @parser.get_date
